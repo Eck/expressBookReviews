4caeb6253319813d0dc910a61851e1af0f20587e
Eck <eck31415@gmail.com>
2024-01-04 13:58:27 -0500

    Changed promises over to async/await for selected methods.

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -8,6 +8,32 @@ const makeStatus = (res, code, message) => {
     return res.status(code).json({message : message});
 }
 
+async function getAllBooks() {
+    let jsonText = JSON.stringify(books, null, 4);
+    return jsonText;
+}
+
+async function getBookByISBN(isbn) {
+    let book = books[isbn];
+    let jsonText = JSON.stringify(book, null, 4);
+
+    return jsonText;
+}
+
+async function getBooksByAuthor(author) {
+    var filtered = Object.fromEntries(Object.entries(books).filter(([k,v]) => v.author === author));
+    let jsonText = JSON.stringify(filtered, null, 4);
+
+    return jsonText;
+}
+
+async function getBooksByTitle(title) {
+    var filtered = Object.fromEntries(Object.entries(books).filter(([k,v]) => v.title === title));
+    let jsonText = JSON.stringify(filtered, null, 4);
+
+    return jsonText;
+}
+
 public_users.post("/register", (req,res) => {
 
     const username = req.body.username;
@@ -27,49 +53,33 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',async function (req, res) {
+    let booksJson = await getAllBooks();
 
-    let getPromise = new Promise((resolve, reject) => {
-            let jsonText = JSON.stringify(books, null, 4);
-            resolve(jsonText);
-    });
-
-    getPromise.then((booksJson) => res.send(booksJson));
+    res.send(booksJson);
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',async function (req, res) {
-    let getPromise = new Promise((resolve, reject) => {
-        let isbn = req.params.isbn;
-        let book = books[isbn];
+    let isbn = req.params.isbn;
+    let bookJson = await getBookByISBN(isbn);
 
-        resolve(JSON.stringify(book, null, 4));
-    });
-
-  getPromise.then((booksJson) => res.send(booksJson));
+    res.send(bookJson);
 });
   
 // Get book details based on author
 public_users.get('/author/:author',async function (req, res) {
-    let getPromise = new Promise((resolve, reject) => {
-        let author = req.params.author;
-        var filtered = Object.fromEntries(Object.entries(books).filter(([k,v]) => v.author === author));
+    let author = req.params.author;
+    let booksJson = await getBooksByAuthor(author);
 
-        resolve(JSON.stringify(filtered, null, 4));
-        });
-
-    getPromise.then((booksJson) => res.send(booksJson));
+    res.send(booksJson);
 });
 
 // Get all books based on title
 public_users.get('/title/:title',async function (req, res) {
-    let getPromise = new Promise((resolve, reject) => {
-        let title = req.params.title;
-        var filtered = Object.fromEntries(Object.entries(books).filter(([k,v]) => v.title === title));
+    let title = req.params.title;
+    let booksJson = await getBooksByTitle(title);
 
-        resolve(JSON.stringify(filtered, null, 4));
-    });
-
-    getPromise.then((booksJson) => res.send(booksJson));
+     res.send(booksJson);
 });
 
 //  Get book review
